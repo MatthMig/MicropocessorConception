ce891ea0b6578bd90bdfbc4c5996d65300b20632
Florence Constans <Florence.Constans@grenoble-inp.org>
2023-05-04 10:08:05 +0200

    commit of CPU_CND

--- vhd/CPU_CND.vhd
+++ vhd/CPU_CND.vhd
@@ -17,10 +17,25 @@ entity CPU_CND is
 end entity;
 
 architecture RTL of CPU_CND is
-
+    signal x, y: unsigned(32 downto 0);
+    signal side_val: std_logic;
+    signal Z, S: std_logic;
 begin
-
     -- Valeurs par défaut pour le passage sur carte, à remplacer
     jcond <= '0';
     slt <= '0';
+
+    side_val <= (not(IR(12)) and not(IR(6))) or (IR(6) and not(IR(13)));
+    x(31 downto 0) <= rs1(31 downto 0);
+    x(32) <= side_val;
+
+    y(31 downto 0) <= alu_y(31 downto 0);
+    y(32) <= side_val;
+
+    Z <= '1' when x=1 else '0';
+
+    S <= '1' when x<y else '0';
+
+    slt <= S;
+    jcond <= ((Z xor IR(12)) and not(IR(14))) or ((S xor IR(12)) and IR(14));
 end architecture;
